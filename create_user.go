a2f2b584534adc4afeed525a42a6f5f11f701d14
Luke Storm <lukeestorm@gmail.com>
2026-01-10 12:40:16 -0700

    add auth package and use on user creation for hashed passwords

--- create_user.go
+++ create_user.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/afleetingchance/Chirpy/internal/auth"
 	"github.com/afleetingchance/Chirpy/internal/database"
 )
 
 func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, req *http.Request) {
 	type parameters struct {
-		Email string `json:"email"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 
 	var params parameters
@@ -20,7 +22,18 @@ func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, req *http.Request
 		return
 	}
 
-	user, err := cfg.db.CreateUser(req.Context(), params.Email)
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Error hashing password: %s", err))
+	}
+
+	user, err := cfg.db.CreateUser(
+		req.Context(),
+		database.CreateUserParams{
+			Email:          params.Email,
+			HashedPassword: hashedPassword,
+		},
+	)
 	if err != nil {
 		respondWithError(w, 500, fmt.Sprintf("Error creating user: %s", err))
 		return
